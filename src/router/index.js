13fd2b7e6d4f051c4eaf5fc3cbd88819cedd428c
qin <2045251631@qq.com>
2023-07-14 11:52:18 +0800

    解决bug，修复路由，解决冲突

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import my from '../views/my/home'
-import securitySetting from "@/views/my/SecuritySetting";
-import PersonalData from "@/views/my/PersonalData";
-import ShippingAddress from "@/views/my/ShippingAddress";
+// import HomeView from '../views/HomeView.vue'
+import profile from '../views/profile/home'
+import securitySetting from "@/views/profile/SecuritySetting";
+import PersonalData from "@/views/profile/PersonalData";
+import ShippingAddress from "@/views/profile/ShippingAddress";
 import order from "@/views/order/order";
 import UserLogin from "@/views/register/UserLogin";
 import UserRegister from "@/views/register/UserRegister";
@@ -12,65 +12,89 @@ import UserRegister from "@/views/register/UserRegister";
 Vue.use(VueRouter)
 
 const routes = [
-    {
-        path: '/',
-        name: 'home',
-        component: HomeView
-    },
+    // {
+    //     path: '/',
+    //     name: 'home',
+    //     component: HomeView
+    // },
     {
         path: '/order',
         name: 'order',
         component: order
     },
+    // {
+    //     path: '/about',
+    //     name: 'about',
+    //     // route level code-splitting
+    //     // this generates a separate chunk (about.[hash].js) for this route
+    //     // which is lazy-loaded when the route is visited.
+    //     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    // },
     {
-        path: '/account-management',
-        name: 'my',
-        component: my,
-        children: [
-            {
-                path: '/account-management/security-setting',
-                name: 'security-setting',
-                component: securitySetting
+        path: '/',
+        name: 'homepage',
+        component: () => import('../views/hongpage/homepage.vue'),
+        redirect: "/frontpage",
+        children: [{
+                path: 'frontpage',
+                name: 'frontpage',
+                component: () => import('../views/hongpage/first_page.vue')
             },
             {
-                path: '/account-management/personal-data',
-                name: 'personal-data',
-                component: PersonalData
+                path: 'goods',
+                name: 'goods',
+                component: () => import('../views/hongpage/goods.vue')
             },
             {
-                path: '/account-management/shipping-address',
-                name: 'shipping-address',
-                component: ShippingAddress
-            }
-
+                path: 'about',
+                name: 'about',
+                component: () => import('../views/hongpage/about.vue')
+            },
+            {
+                path: 'productdetails',
+                name: 'productdetails',
+                component: () => import('../views/hongpage/product_details.vue')
+            },
+            {
+                path: 'test',
+                name: 'test',
+                component: () => import('../views/hongpage/test.vue')
+            },
+            {
+                path: '/account-management',
+                name: 'profile',
+                component: profile,
+                // redirect: "/security_setting",
+                children: [{
+                        path: '/account-management/security-setting',
+                        name: 'security-setting',
+                        component: securitySetting
+                    },
+                    {
+                        path: '/account-management/personal-data',
+                        name: 'personal-data',
+                        component: PersonalData
+                    },
+                    {
+                        path: '/account-management/shipping-address',
+                        name: 'shipping-address',
+                        component: ShippingAddress
+                    }
+        
+                ]
+            },
         ]
     },
     {
-        path: '/about',
-        name: 'about',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+        path: '/register',
+        name: '注册',
+        component: UserRegister
+    },
+    {
+        path: '/login',
+        name: '登录',
+        component: UserLogin
     }
-  {
-    path:'/',
-    name:'homepage',
-    component:()=>import('../views/hongpage/homepage.vue'),
-    redirect:"/frontpage",
-    children:[
-      {path:'frontpage',name:'frontpage',component:()=>import('../views/hongpage/first_page.vue')},
-      {path:'goods',name:'goods',component:()=>import('../views/hongpage/goods.vue')},
-      {path:'about',name:'about',component:()=>import('../views/hongpage/about.vue')},
-      {path: 'productdetails',name:'productdetails',component:()=>import('../views/hongpage/product_details.vue')},
-      {path: 'test',name:'test',component:()=>import('../views/hongpage/test.vue')}
-    ]
-  },
-  {
-    path: '/register',
-    name:'注册',
-    component: UserRegister
-  }
 
 ]
 
@@ -80,4 +104,4 @@ const router = new VueRouter({
     routes
 })
 
-export default router
+export default router;
