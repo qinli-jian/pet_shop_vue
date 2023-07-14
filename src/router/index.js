1daf0aaa4ee5d3cca53c550af0f271a16c129aa7
XiaoJiang <2894745898@qq.com>
2023-07-14 11:48:01 +0800

    完成部分订单详情、购物车、重设密码、申请售后

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,11 @@ import VueRouter from 'vue-router'
 
 import UserLogin from "@/views/register/UserLogin";
 import UserRegister from "@/views/register/UserRegister";
-
+import UserForget from "@/views/register/UserForget";
+import ShoppingCart from "@/views/ShoppingCart/ShoppingCart"
+import home from "@/views/ShoppingCart/home";
+import OrderDetails from "@/views/OrderDetails/OrderDetails";
+import ApplyAfterSales from "@/views/ApplyAfterSales/ApplyAfterSales";
 
 Vue.use(VueRouter)
 
@@ -11,7 +15,12 @@ const routes = [
   {
     path: '/',
     name: '主页',
-    redirect: ('/register')
+    redirect: ('/ApplyAfterSales')
+  },
+  {
+    path:'/ShoppingCart',
+    name:'购物车',
+    component: ShoppingCart
   },
   {
     path: '/UserLogin',
@@ -19,9 +28,29 @@ const routes = [
     component: UserLogin
   },
   {
-    path: '/register',
+    path: '/UserRegister',
     name:'注册',
     component: UserRegister
+  },
+  {
+    path:'/UserForget',
+    name: '忘记密码',
+    component: UserForget
+  },
+  {
+    path:'/home',
+    name:'jjj',
+    component: home
+  },
+  {
+    path:'/OrderDetails',
+    name:'订单详情页',
+    component: OrderDetails
+  },
+  {
+    path:'/ApplyAfterSales',
+    name:'申请售后',
+    component: ApplyAfterSales
   }
 
 
